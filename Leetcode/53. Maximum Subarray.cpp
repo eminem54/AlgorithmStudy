cd9ed16cc4ebd8d16d950c52876a303b8882de47
EasyZero <eminem54@naver.com>
2019-02-09 23:46:22 +0900

    2018-02-09

---
+++ Leetcode/53. Maximum Subarray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int maxv = -987654321;
+        int cur = 0;
+        for(int i=1; i<=nums.size(); i++){
+            cur = max(nums[i], cur + nums[i]);
+            maxv = max(maxv, cur);
+        }
+        return maxv;
+    }
+};
+
+int main(){
+    Solution sol = Solution();
+    vector<int> a = {-2,1,-3,4,-1,2,1,-5,4};
+    cout<< sol.maxSubArray(a);
+    return 0;
+}
